0fe104f32d561464775b1be1f21cf952613d966b
Raul Castro <71842855+raul-castro@users.noreply.github.com>
2024-01-07 18:40:48 +0100

    Some basics

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -17,41 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = 'night',
-    },
-  },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-
-}
-
-
 local opts = {}
 
-require("lazy").setup(plugins, opts)
-
-local builtin =  require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader><leader>', builtin.live_grep, {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
-
-require("tokyonight").setup()
-vim.cmd.colorscheme "tokyonight-night"
-
+require("vim-options")
+require("lazy").setup("plugins")
 
