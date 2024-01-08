72bab55c817d473f8cb04304e6f2651f19474009
Raul Castro <71842855+raul-castro@users.noreply.github.com>
2024-01-08 01:40:20 +0100

    formatting and autocompletion

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -1,54 +1,70 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "tsserver", "clangd", "awk_ls", "astro", "bashls", "cssls", "dockerls", "elixirls", "elp", "gopls", "html", "htmx", "jdtls", "ltex", "matlab_ls", "pyright", "sqls", "svelte", "taplo", "tailwindcss"}
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.awk_ls.setup({})
-      lspconfig.astro.setup({})
-      lspconfig.bashls.setup({})
-      lspconfig.cssls.setup({})
-      lspconfig.dockerls.setup({})
-      lspconfig.elixirls.setup({})
-      lspconfig.elp.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.htmx.setup({})
-      lspconfig.jdtls.setup({})
-      lspconfig.ltex.setup({})
-      lspconfig.matlab_ls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.sqls.setup({})
-      lspconfig.svelte.setup({})
-      lspconfig.taplo.setup({})
-      lspconfig.tailwindcss.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"clangd",
+					"astro",
+					"bashls",
+					"cssls",
+					"dockerls",
+					"elixirls",
+					"elp",
+					"html",
+					"jdtls",
+					"ltex",
+					"matlab_ls",
+					"pyright",
+					"sqlls",
+					"svelte",
+					"taplo",
+					"tailwindcss",
+					"rust_analyzer",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.astro.setup({})
+			lspconfig.bashls.setup({})
+			lspconfig.cssls.setup({})
+			lspconfig.dockerls.setup({})
+			lspconfig.elixirls.setup({})
+			lspconfig.elp.setup({})
+			lspconfig.html.setup({})
+			lspconfig.jdtls.setup({})
+			lspconfig.ltex.setup({})
+			lspconfig.matlab_ls.setup({})
+			lspconfig.pyright.setup({})
+			lspconfig.svelte.setup({})
+			lspconfig.taplo.setup({})
+			lspconfig.tailwindcss.setup({})
+			lspconfig.rust_analyzer.setup({})
+			lspconfig.sqlls.setup({})
 
-
-
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-
-
-    end
-
-  }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
-
