d4fad914b25150636a4c812ad85c5b154eed97f9
raul-castro <71842855+raul-castro@users.noreply.github.com>
2024-01-23 01:24:45 +0100

    Added wezterm terminal emulator's config
    Still be there alacritty's configs but not used

--- dot_config/nvim/lua/plugins/tokyonight.lua
+++ dot_config/nvim/lua/plugins/tokyonight.lua
@@ -1,9 +1,15 @@
 return {
   "folke/tokyonight.nvim",
+  transparent = true,
   lazy = false,
   priority = 1000,
   opts = {
     style = 'night',
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
   },
   config = function()
     vim.cmd.colorscheme "tokyonight-night"
