e29be2f7f6a7eae7b5479dc3b0be72b21fcb5591
raul-castro <71842855+raul-castro@users.noreply.github.com>
2024-05-08 23:46:46 +0200

    deleted, for now nvim config and changed pager

--- dot_config/nvim/lua/custom/plugins/oil.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "stevearc/oil.nvim",
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    { "-", "<cmd>Oil --float<cr>", desc = "Oil file explorer" },
-  },
-}
