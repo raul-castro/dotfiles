ad782906d398530c463752cbaf2069e4d1a4fe7d
raul-castro <71842855+raul-castro@users.noreply.github.com>
2024-08-08 15:07:37 +0200

    Deletet colors dir

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -11,7 +11,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -20,10 +20,11 @@ end
 -- config.colors = colors
 config.color_scheme = "tokyonight_night"
 -- Font
-config.font = wezterm.font_with_fallback {
-  "JetBrainsMono Nerd Font",
-  "Apple Color Emoji"
-}
+config.font = wezterm.font_with_fallback({
+	"Iosevka Nerd Font Propo",
+	"Apple Color Emoji",
+	"Menlo",
+})
 -- config.font = font.font
 -- config.font_size = font.size
 config.font_size = 14
@@ -35,7 +36,6 @@ config.window_background_opacity = 0.96
 
 -- Hide the window decoration
 config.window_decorations = "RESIZE"
-
 
 -- and finally, return the configuration to wezterm
 return config
